a17dd66938e3e2cab2abd67c986fae015a179914
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-05-07 00:17:17 -0400

    Simplify generate script by loading script from CDN

--- generate.js
+++ generate.js
@@ -1,35 +1,36 @@
-const _ = require("lodash");
 const fs = require("fs");
+const _ = require("lodash");
 const jsdom = require("jsdom");
+
 const { JSDOM } = jsdom;
-const mockAudioApi = require("./mock-audio-api");
-
-const p5Source = file => {
-  return fs.readFileSync("./node_modules/p5/lib/" + file, "utf8");
-};
-const tag = (tagName, src) => {
-  return "<" + tagName + ">" + src + "</" + tagName + ">";
-};
-const html = tag.bind(null, "html");
-const script = tag.bind(null, "script");
-
 const { window } = new JSDOM(
-  `${script("document.hasFocus = function() {};")} ${script(
-    p5Source("p5.js")
-  )}`,
-  { runScripts: "dangerously" }
+  `
+    <!doctype html>
+      <html>
+        <head>
+          <script>document.hasFocus = function() {};</script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.js"></script>
+        </head>
+        <body></body>
+      </html>
+    `,
+  { runScripts: "dangerously", resources: "usable" }
 );
 
-window.addEventListener("error", function(event) {
+window.addEventListener("error", event => {
   console.log(event);
   console.error("script error!", event.error, event.error.stack);
 });
 
-const getGlobals = instance => {
-  return _.keysIn(instance).filter(function(key) {
-    return key[0] !== "_";
-  });
-};
+window.addEventListener("load", event => {
+  const p5 = window.p5;
+  const keys = getGlobals(new p5());
+  writeGlobals("globals.js", keys);
+
+  process.exit(0);
+});
+
+const getGlobals = instance => _.keysIn(instance).filter(key => key[0] !== "_");
 
 const writeGlobals = (filename, keys) => {
   const globals = _.zipObject(keys, _.fill(Array(keys.length), true));
@@ -37,16 +38,3 @@ const writeGlobals = (filename, keys) => {
     "module.exports = " + JSON.stringify(globals, null, "  ") + ";\n";
   fs.writeFileSync(filename, output);
 };
-
-window.addEventListener("load", event => {
-  const p5 = window.p5;
-  const keys = getGlobals(new p5());
-  writeGlobals("globals.js", keys);
-
-  // _.extend(window, mockAudioApi);
-  // window.document.write(script(p5Source("addons/p5.sound.js")));
-  // var soundkeys = _.difference(getGlobals(new p5()), keys, domkeys);
-  // writeGlobals("globals.sound.js", soundkeys);
-
-  process.exit(0);
-});
